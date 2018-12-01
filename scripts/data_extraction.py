32d8cc65005650192dfc4ab6ac184d982cc20b21
Jason Chang <jasonchang0@berkeley.edu>
2018-12-01 05:05:03 -0500

    repo organization

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -1,1 +1,15 @@
 import pandas as pd
+import json
+import os
+import glob
+
+os.chdir('../data')
+
+# reading the JSON data using json.load()
+file = 'data.json'
+with open(file) as train_file:
+    dict_train = json.load(train_file)
+
+# converting json dataset from dictionary to dataframe
+train = pd.DataFrame.from_dict(dict_train, orient='index')
+train.reset_index(level=0, inplace=True)
