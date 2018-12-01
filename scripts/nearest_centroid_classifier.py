59f0246b691591a910f4bde8b751c39c7428fd08
Jason Chang <jasonchang0@berkeley.edu>
2018-12-01 14:45:34 -0500

    classified US airports

--- scripts/nearest_centroid_classifier.py
+++ scripts/nearest_centroid_classifier.py
@@ -1,5 +1,6 @@
 from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.neighbors import nearest_centroid
+from sklearn.neighbors import NearestCentroid
+from sklearn import preprocessing
 import pandas as pd
 import numpy as np
 import os
@@ -9,16 +10,40 @@ os.chdir('../data')
 
 df = pd.read_csv('airport_features.csv')
 
-train_x = df[:6]
-train_x_airports = train_x['Origin_airport']
-train_x.drop('Origin_airport', axis=1, inplace=True)
+airports = df['Origin_airport']
+x = df.drop('Origin_airport', axis=1, inplace=False)
 
-test_x = df[6:]
-test_x_airports = test_x['Origin_airport']
-test_x.drop('Origin_airport', axis=1, inplace=True)
+scaler = preprocessing.StandardScaler()
+x = scaler.fit_transform(x)
 
-print(train_x.head())
-print(test_x.head())
+train_x = x[:6]
+# train_x_airports = train_x['Origin_airport']
+# train_x.drop('Origin_airport', axis=1, inplace=True)
+
+test_x = x[6:]
+# test_x_airports = test_x['Origin_airport']
+# test_x.drop('Origin_airport', axis=1, inplace=True)
+
+print(train_x[:5])
+print(test_x[:5])
+
+train_y = [_ for _ in range(6)]
+
+clf = NearestCentroid(metric='cosine')
+clf.fit(train_x, train_y)
+
+test_y = clf.predict(test_x)
+
+# x = x.insert(0, 'Origin_airport', airports)
+
+print(train_y)
+print(test_y)
+
+df['Labels'] = list(np.array(train_y).flatten()) + list(np.array(test_y).flatten())
+print(df.head())
+
+df.to_csv('airport_features.csv', index=False)
+
 
 
 
